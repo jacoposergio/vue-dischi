cd391326077562490e24b29057d634379960177e
jacoposergio <djago32@gmail.com>
2022-07-08 17:03:42 +0200

    header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    sono un header
+    <div class="spotify-logo">
+        logo
+    </div>
   </header>
 </template>
 
@@ -10,6 +12,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import'@/assets/scss/vars.scss';
+
+    header{
+        height: 50px;
+        background-color: $bg_first_color;
+        
+    }
 
 </style>
