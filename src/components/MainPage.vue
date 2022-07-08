3f18eb2a391e7e738555f405d441ceb4ba0e3b76
jacoposergio <djago32@gmail.com>
2022-07-08 18:52:29 +0200

    problem

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,25 +2,10 @@
   <main>
     <div class="main-wrapper">
         <div class="container">
-            <div class="row row-cols-5">
-                <div class="col">
-                    <div class="card">carta</div>
+            <div class="row row-cols-md-5 row-cols-sm-3  ">
+                <div v-for="song in songList " class="col" :key="index" :productDetails="element" >
+                    <SongCard/>
                 </div> 
-                 <div class="col">
-                    <div class="card">carta</div>
-                </div>
-                 <div class="col">
-                    <div class="card">carta</div>
-                </div>
-                 <div class="col">
-                    <div class="card">carta</div>
-                </div>
-                 <div class="col">
-                    <div class="card">carta</div>
-                </div>
-                 <div class="col">
-                    <div class="card">carta</div>
-                </div>
             </div>
         </div>
     </div>
@@ -28,20 +13,26 @@
 </template>
 
 <script>
-export default {
+import SongCard from './SongCard.vue';
 
+export default {
+   name: "MainPage",
+    components: {
+        SongCard
+    },
 }
 </script>
 
 
 <style lang="scss" scoped>
+@import'@/assets/scss/vars.scss';
+
 
 main{
     .main-wrapper{
         margin: 50px auto;
         width: 70%;
     }
-
 }
 
 </style>
---
+++ src/components/SongCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card">
+    <div class="song-img">
+        <img src="r" alt="">
+    </div>
+    <h3>{{song.title}}</h3>
+    <h4>Artist</h4>
+    <h5>data</h5>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "SongCard",
+
+   data(){
+       return{
+           url: "https://flynn.boolean.careers/exercises/api/array/music",
+           songList:[],
+       }
+   },
+
+   created() {
+    this.getSongs();
+   },
+
+   methods:{
+    getSongs(){
+        axios.get(this.url).then( (result) =>{
+            this.songList = result.data.results
+        })
+        .catch((err)=>{
+             console.log("Error",err);
+        });
+    }
+   }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import'@/assets/scss/vars.scss';
+
+.card{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem;
+        background-color: lightcyan;
+        text-align: center;
+        margin-bottom: 1rem;
+
+        .song-img{
+            width: 100%;
+            aspect-ratio: 1/1;
+            background-color: black;
+        }
+
+        h3{
+            width: 100%;
+            font-size: 1.5rem;
+            padding: 0.5rem 0;
+            margin-bottom: 0;
+        }
+
+        h4{
+            width: 100%;
+            font-size: 1.2rem;
+            color: $subtext_color;
+            margin-bottom: 0;
+        }
+
+        h5{
+            width: 100%;
+             font-size: 1rem;
+            color: $subtext_color;
+        }
+    }
+
+</style>
