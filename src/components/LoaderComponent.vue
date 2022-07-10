7e01a644310ca0efbb3d27684188e91654b124be
jacoposergio <djago32@gmail.com>
2022-07-10 15:13:03 +0200

    bonus done

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="loader">
+     <div class="text">
+        loading...
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "LoaderComponent",
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import'@/assets/scss/vars.scss';
+
+    .loader{
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: #fefefe;
+         animation-name: pulse;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+
+ 
+
+        .text{
+            text-transform: uppercase;
+            letter-spacing: 2px;
+        }
+    }
+
+           @keyframes pulse {
+             0%   {transform: scale(1);}
+            25%  {transform: scale(1.3);}
+            50%  {transform: scale(1);}
+            75%  {transform: scale(1.3);}
+            100% {transform: scale(1);}       
+        }
+
+</style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,31 +1,35 @@
 <template>
   <main>
-    <div class="main-wrapper">
-        <div class="container">
-            <div class="row row-cols-md-5 row-cols-sm-3  ">
-                <div v-for="song, index in songList " class="col" :key="index" :element="song" >
+      <div v-if="isLoaded" class="main-wrapper">
+          <div class="container">
+              <div class="row row-cols-md-5 row-cols-sm-3  ">
+                  <div v-for="song, index in songList " class="col" :key="index" :element="song" >
                     <SongCard :info="song" />
                 </div> 
             </div>
         </div>
     </div>
+        <LoaderComponent v-else />
   </main>
 </template>
 
 <script>
 import axios from "axios";
 import SongCard from './SongCard.vue';
+import LoaderComponent from './LoaderComponent.vue';
 
 export default {
    name: "MainPage",
     components: {
-        SongCard
+        SongCard,
+        LoaderComponent
     },
     
    data(){
        return{
            url: "https://flynn.boolean.careers/exercises/api/array/music",
            songList:[],
+           isLoaded: false
        }
    },
      created() {
@@ -36,6 +40,7 @@ export default {
     getSongs(){
         axios.get(this.url).then( (result) =>{
             this.songList = result.data.response;
+            this.isLoaded = true;
         })
         .catch((err)=>{
              console.log("Error",err);
